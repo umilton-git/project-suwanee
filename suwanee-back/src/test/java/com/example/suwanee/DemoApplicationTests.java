fcdd91ea7ec3cdc0c05b04703d19a46d1ce01ca4
Illiad <ulysses.github@protonmail.com>
2026-05-15 01:26:00 -0400

    Created docker-compose file for backend Postgres container

--- suwanee-back/src/test/java/com/example/suwanee/DemoApplicationTests.java
+++ suwanee-back/src/test/java/com/example/suwanee/DemoApplicationTests.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration"
+})
 class DemoApplicationTests {
 
 	@Test
