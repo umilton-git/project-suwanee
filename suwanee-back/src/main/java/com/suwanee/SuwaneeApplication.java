cf3d6af2e55b5c22b70dcb49f99e9604d6e68d58
Illiad <ulysses.github@protonmail.com>
2026-05-16 19:20:42 -0400

    Fixes for proper building

---
+++ suwanee-back/src/main/java/com/suwanee/SuwaneeApplication.java
@@ -0,0 +1,12 @@
+package com.suwanee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuwaneeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuwaneeApplication.class, args);
+    }
+}
--- suwanee-back/src/test/java/com/example/suwanee/DemoApplicationTests.java
+++ suwanee-back/src/test/java/com/example/suwanee/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.suwanee;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
