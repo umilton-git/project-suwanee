18881dd7524556d5fb58e406fb0fc290fb8b8729
Illiad <ulysses.github@protonmail.com>
2026-05-18 00:21:44 -0400

    Fixed UpdateEntryRequest

--- suwanee-back/src/main/java/com/suwanee/dto/request/UpdateEntryRequest.java
+++ suwanee-back/src/main/java/com/suwanee/dto/request/UpdateEntryRequest.java
@@ -1,6 +1,16 @@
+package com.suwanee.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import java.util.Optional;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class UpdateEntryRequest {
-    private Optional<String> learned;
-    private Optional<String> insights;
-    private Optional<String> questions;
-    private Optional<Integer> confidence;
+    private Optional<String> learned = Optional.empty();
+    private Optional<String> insights = Optional.empty();
+    private Optional<String> questions = Optional.empty();
+    private Optional<Integer> confidence = Optional.empty();
 }
