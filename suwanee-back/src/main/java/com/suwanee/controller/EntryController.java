e09ff713ed8c34d9111db101789e274004f29f5d
Illiad <ulysses.github@protonmail.com>
2026-05-22 01:53:12 -0400

    Created Entry Controller

---
+++ suwanee-back/src/main/java/com/suwanee/controller/EntryController.java
@@ -0,0 +1,45 @@
+package com.suwanee.controller;
+
+import com.suwanee.dto.request.CreateEntryRequest;
+import com.suwanee.dto.request.UpdateEntryRequest;
+import com.suwanee.dto.response.EntryResponse;
+import com.suwanee.service.EntryService;
+import com.suwanee.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/topics/{topicId}/entries")
+public class EntryController {
+
+    private final EntryService entryService;
+    private final UserService userService;
+
+    @PostMapping
+    public EntryResponse createEntry(@PathVariable UUID topicId, @Valid @RequestBody CreateEntryRequest createEntryRequest) {
+        UUID userId = userService.getCurrentUserId();
+        return entryService.createEntry(topicId, userId, createEntryRequest);
+    }
+
+    @GetMapping
+    public List<EntryResponse> getEntries(@PathVariable UUID topicId) {
+        return entryService.getEntriesForTopic(topicId, userService.getCurrentUserId());
+    }
+
+    @PatchMapping("/{entryId}")
+    public EntryResponse updateEntry(@PathVariable UUID topicId, @PathVariable UUID entryId, @Valid @RequestBody UpdateEntryRequest updateEntryRequest) {
+        UUID userId = userService.getCurrentUserId();
+        return entryService.updateEntry(entryId, userId, updateEntryRequest);
+    }
+
+    @DeleteMapping("/{entryId}")
+    public void deleteEntry(@PathVariable UUID topicId, @PathVariable UUID entryId) {
+        UUID userId = userService.getCurrentUserId();
+        entryService.deleteEntry(entryId, userId);
+    }
+}
